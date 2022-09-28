92a8f45c4dfa6f224faaed95b14a7661a52ea3f8
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-09-28 22:11:14 +0500

    generateToken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,6 +76,13 @@ const getUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Current User Data" });
 });
 
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
 module.exports = {
   registerUser,
   loginUser,
