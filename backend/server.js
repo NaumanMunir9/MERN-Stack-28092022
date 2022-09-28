0b8b28cd1d4d6d310b9f98a0dcfb826c331ff6a7
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-09-28 10:43:21 +0500

    middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
+// libraries
 const express = require("express");
 const dotenv = require("dotenv").config();
+// errorMiddleware
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+// errorHandler Middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
