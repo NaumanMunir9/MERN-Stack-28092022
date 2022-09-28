6e9ff7c7145bf5b41f00b55a08561f6a63e4a7d1
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-09-28 10:19:39 +0500

    modularize to goalController and clean up

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,28 +1,15 @@
+// libraries
 const express = require("express");
 const router = express.Router();
+// controllers
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Get Goals!",
-  });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({
-    message: "Set Goal!",
-  });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({
-    message: `Update Goal ${req.params.id}`,
-  });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({
-    message: `Delete Goal ${req.params.id}`,
-  });
-});
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
