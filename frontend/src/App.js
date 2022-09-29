7cb266790dc228dec0f8f664247284ae78b8d546
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-09-29 15:37:49 +0500

    ToastContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 // libraries
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// components
 import Header from "./components/Header";
 // pages
 import Dashboard from "./pages/Dashboard";
@@ -19,6 +22,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
