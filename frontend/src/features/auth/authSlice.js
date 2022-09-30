2894636f37d22f135df924e2581b06ed6f3f1857
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-09-30 10:16:18 +0500

    logout

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -32,6 +32,11 @@ export const register = createAsyncThunk(
   }
 );
 
+// logout
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
